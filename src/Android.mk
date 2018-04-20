c4573a54db799f32950e96331b9a409f8f2f49ad
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2018-04-20 06:54:31 -0500

    Put the built executable into the recovery image
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- src/Android.mk
+++ src/Android.mk
@@ -26,6 +26,7 @@ LOCAL_CFLAGS := -Wall
 LOCAL_LDFLAGS := -Wl,-dynamic-linker,/sbin/linker64
 LOCAL_LDLIBS := -lz
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_EXECUTABLE)
 
 
